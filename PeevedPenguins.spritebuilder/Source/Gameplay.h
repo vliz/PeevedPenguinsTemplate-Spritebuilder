126cb457dbc27fcab2ad22d79a2c0da82be4d386
Agnes Felicia <Liz@Agness-MacBook-Air.local>
2014-07-25 22:57:49 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,14 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Agnes Felicia on 7/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+
+@end
